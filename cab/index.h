dde61e4b1035ab40811a9659d7301802061d7153
pedro manuel <pedmanue14@gmail.com>
2025-02-21 22:05:41 +0100

    adicionando bibliotecas SDL_image, SDL_ttf e SDL_mixer ao arquivo index.h

--- cab/index.h
+++ cab/index.h
@@ -2,6 +2,9 @@
 #define INDEX_H
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
